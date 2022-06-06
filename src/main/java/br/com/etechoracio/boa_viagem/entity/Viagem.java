2955460b994010a4bec2dce72d9766a558f675da
danbatistaskt <danbatistask8@gmail.com>
2022-06-06 09:06:36 -0300

    atualizacao projeto

--- src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
+++ src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
@@ -2,18 +2,50 @@ package br.com.etechoracio.boa_viagem.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+
+import br.com.etechoracio.boa_viagem.enums.TipoViagemEnum;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name="TBL_VIAGEM")
 
 public class Viagem {
-	private Long id;
-	private String destino;
-	private String tipo;
-	private LocalDate chegada;
-	private LocalDate saida;
-	private Double orcamento;
-	private Integer pessoas;
-}
+
+	 @Id
+	 @Column(name="ID_VIAGEM")
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	 
+	 @Column(name="TX_DESTINO")
+	 private String destino;
+	 
+	 @Enumerated(EnumType.STRING)
+	 @Column(name="TP_VIAGEM")
+	 private TipoViagemEnum tipo;
+	 
+	 @Column(name="DT_CHEGADA")
+	 private LocalDate chegada;
+	 
+	 @Column(name="DT_SAIDA")
+	 private LocalDate saida;
+	 
+	 @DecimalMin(value="0.1")
+	 @Column(name="VLR_ORCAMENTO")
+	 private Double orcamento;
+	 
+	 @DecimalMin(value="1")
+	 @Column(name="QTD_PESSOAS")
+	 private Integer pessoas;
+	}
---
+++ src/main/java/br/com/etechoracio/boa_viagem/service/GastoService.java
@@ -0,0 +1,84 @@
+	package br.com.etechoracio.boa_viagem.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.etechoracio.boa_viagem.entity.Gasto;
+import br.com.etechoracio.boa_viagem.entity.Viagem;
+import br.com.etechoracio.boa_viagem.repository.GastoRepository;
+import br.com.etechoracio.boa_viagem.repository.ViagemRepository;
+
+
+@Service
+public class GastoService {
+
+	@Autowired
+	private GastoRepository repository;
+	
+	@Autowired
+	private ViagemRepository viagemRepo;
+	
+	public List<Gasto> listarTodos(){
+		return repository.findAll();
+	}
+	
+	public Optional<Gasto> buscarPorId(Long id) {
+		
+
+		 return repository.findById(id);
+	}
+	
+	public boolean deletarPorId(Long id) {
+		boolean existe = repository.existsById(id);
+		
+		if (existe) {repository.deleteById(id);}
+		return existe;
+			
+	}
+	
+	public Gasto inserir(Gasto obj) {
+		
+		
+		Optional<Viagem> existe = viagemRepo.findById(obj.getViagem().getId());
+		if(!existe.isPresent()) {
+
+			throw new RuntimeException("Viagem não encontrada.");
+		}
+		
+		if(existe.get().getSaida() != null) {
+			throw new RuntimeException("Data de viagem já foi encerrada.");
+		}
+		
+		if(obj.getData().isBefore(existe.get().getChegada())) {	// Verifica se data de gasto é depois de data de chegada da viagem.
+			throw new RuntimeException("Erro. A data de viagem já foi encerrada.");
+		}
+		
+		// ... 
+		
+		return repository.save(obj);
+	}
+	
+	
+	
+	public Optional<Gasto> atualizar(Long id, Gasto gasto) {
+		
+		Optional<Viagem> viagem = viagemRepo.findById(gasto.getViagem().getId());
+		
+		if(viagem.get().getId() != gasto.getViagem().getId()) {
+			
+			throw new RuntimeException("Viagem inserida não corresponde com a viagem de atualização.");
+		}
+		
+		boolean existe = repository.existsById(id);
+		if (!existe) {return Optional.empty();}
+		
+		if (gasto.getData().isBefore(viagem.get().getChegada())) {
+			
+			throw new RuntimeException("Não foi possível atualizar o gasto, pois a data do gasto é anterior ao da viagem.");
+		}
+		return Optional.of(repository.save(gasto));
+	}
+}
